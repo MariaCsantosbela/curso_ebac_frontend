9316f8ce76d156e76a48caacf992dc9496eae345
MariaCsantosbela <104848675+MariaCsantosbela@users.noreply.github.com>
2024-02-25 00:05:29 -0300

    Add files via upload

---
+++ exercicio_plugins_jquery/js/main.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+    $(`#carrossel-imagens`).slick({autoplay: true, });
+
+    $('.menu-hamburguer').click(function(){
+        $(`nav`).slideToggle();
+
+    })
+
+    $(`#telefone`).mask(`(00) 00000-0000 `)
+
+    $(`form`).validate({
+        rules:{
+            nome: {
+                required : true
+            },
+            email: {
+                required : true,
+                email: true
+            },
+            telefone: {
+                required : true
+            },
+            veiculoInteresse: {
+                required: true
+            },
+            mensagem : {
+                required : true
+            },
+
+            
+        },
+        messages : {
+            nome: 'Por favor, insira o seu nome '
+        },
+        submitHandler : function(form){
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador){
+            let camposIncorretos = validador.numberOfInvalids();
+            if(camposIncorretos){
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
+    })
+    $(`.lista-veiculos button`).click(function(){
+        const destino = $('#contato');
+        const nomeVeiculo = $(this).parent().find(`h3`).text();
+        $(`#veiculo-interese`).val(nomeVeiculo );
+
+        
+
+        $(`html`).animat({
+            scrollTop: destino.offset().top 
+        }, 1000)
+    })
+
+
+
+})
